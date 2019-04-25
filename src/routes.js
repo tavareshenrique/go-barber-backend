f70b7fd3f57a752736e577b872b08954e93a5015
Henrique Tavares <ihenrits@gmail.com>
2019-04-25 00:04:29 -0300

    Foto de Perfil na Dashboard

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ const guestMiddleware = require('./app/middlewares/guest')
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 const DashboardController = require('./app/controllers/DashboardController')
+const FileController = require('./app/controllers/FileController')
 
 routes.use((req, res, next) => {
   res.locals.flashSucces = req.flash('success')
@@ -17,6 +18,8 @@ routes.use((req, res, next) => {
 
   return next()
 })
+
+routes.get('/files/:file', FileController.show)
 
 routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
