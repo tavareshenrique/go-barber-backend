fdd44e96ce2cf57dc3400a7704e45e5bc35824be
Henrique Tavares <ihenrits@gmail.com>
2019-04-24 22:38:49 -0300

    Validaçao para Logar no Sistema

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,26 @@
+const { User } = require('../models')
+
+class SessionController {
+  async create (rea, res) {
+    return res.render('auth/signin')
+  }
+
+  async store (req, res) {
+    const { email, password } = req.body
+    const user = await User.findOne({ where: { email } })
+
+    if (!user) {
+      console.log('Usuário não encontrado')
+      return res.redirect('/')
+    }
+
+    if (!(await user.checkPassword(password))) {
+      console.log('Senha incorreta')
+      return res.redirect('/')
+    }
+
+    return res.redirect('/app/dashboard')
+  }
+}
+
+module.exports = new SessionController()
--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,14 @@ const upload = require('multer')(multerConfig)
 const routes = express.Router()
 
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
+
+routes.get('/', SessionController.create)
+routes.post('/signin', SessionController.store)
 
 routes.get('/signup', UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.get('/app/dashboard', (req, res) => res.render('dashboard'))
 
 module.exports = routes
