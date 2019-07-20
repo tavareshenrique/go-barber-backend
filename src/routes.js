4d0293b143b7808ea4da5915adc22adce09a8b9f
Henrique Tavares <ihenrits@gmail.com>
2019-07-20 16:52:47 -0300

    Cadastro de Usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Henrique Tavares',
-    email: 'ihenrit@gmail.com',
-    password_hash: '123123456',
-    provider: false,
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
