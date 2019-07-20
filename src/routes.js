410edd74beafe56467d4e658de148cc65ca5db0a
Henrique Tavares <ihenrits@gmail.com>
2019-07-20 16:41:26 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Henrique' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Henrique Tavares',
+    email: 'ihenrit@gmail.com',
+    password_hash: '123123456',
+    provider: false,
+  });
+
+  return res.json(user);
 });
 
 export default routes;
